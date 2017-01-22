0b095bfc825b001fcbfc8340161967f4cad3a440
Guillaume\Munsch <guigui.munsch@gmail.com>
2017-01-22 14:12:15 +0100

    Connect view, FlatterFX

---
+++ FeedReader/src/main/java/com/munsch/feedreader/MainApp.java
@@ -0,0 +1,39 @@
+package com.munsch.feedreader;
+
+import com.guigarage.flatterfx.FlatterFX;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        
+        Scene scene = new Scene(root);
+//        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+        FlatterFX.style();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
